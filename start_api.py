aec056f3df8425d03f7a49e7bef286d3ec47b42a
João Miguel Anacleto <111027638+jmanacleto@users.noreply.github.com>
2024-10-17 17:09:47 -0300

    Delete start_api.py

--- start_api.py
+++
@@ -1,9 +0,0 @@
-# A Generativa e RAG - Frontend
-# Módulo de Inicialização da API
-
-# Import
-import uvicorn
-
-# Inicializa a API
-if __name__=="__main__":
-    uvicorn.run("api:app", host = '0.0.0.0', port = 8000, reload = False,  workers = 3)
